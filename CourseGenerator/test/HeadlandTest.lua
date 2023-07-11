2ba361c4efcde4ae6c82d6f13418ea8ad8b8bd83
Peter Vaiko <peter.vaiko@siemens.com>
2023-07-11 08:10:53 -0400

    test: make sure tests return error on failure

--- CourseGenerator/test/HeadlandTest.lua
+++ CourseGenerator/test/HeadlandTest.lua
@@ -1,11 +1,13 @@
 require('include')
 lu.EPS = 0.01
-
-lu.assertAlmostEquals(cg.Headland._getTransitionLength(10, 5), 10)
-lu.assertAlmostEquals(cg.Headland._getTransitionLength(10, 5.1), 10.2)
-lu.assertAlmostEquals(cg.Headland._getTransitionLength(10, 4.9), 9.8)
-lu.assertAlmostEquals(cg.Headland._getTransitionLength(10, 4.999), 10)
-lu.assertAlmostEquals(cg.Headland._getTransitionLength(10, 10), 2 * 10 * math.cos(math.rad(30)))
-lu.assertAlmostEquals(cg.Headland._getTransitionLength(3, 6), 7.93)
-lu.assertAlmostEquals(cg.Headland._getTransitionLength(3, 5), 7.14)
+function testHeadland()
+    lu.assertAlmostEquals(cg.Headland._getTransitionLength(10, 5), 10)
+    lu.assertAlmostEquals(cg.Headland._getTransitionLength(10, 5.1), 10.2)
+    lu.assertAlmostEquals(cg.Headland._getTransitionLength(10, 4.9), 9.8)
+    lu.assertAlmostEquals(cg.Headland._getTransitionLength(10, 4.999), 10)
+    lu.assertAlmostEquals(cg.Headland._getTransitionLength(10, 10), 2 * 10 * math.cos(math.rad(30)))
+    lu.assertAlmostEquals(cg.Headland._getTransitionLength(3, 6), 7.93)
+    lu.assertAlmostEquals(cg.Headland._getTransitionLength(3, 5), 7.14)
+end
+os.exit(lu.LuaUnit.run())
 
--- CourseGenerator/test/RowPatternTest.lua
+++ CourseGenerator/test/RowPatternTest.lua
@@ -1,137 +1,139 @@
 require('include')
 lu.EPS = 0.01
+function testRowPattern()
+    local rows = {}
+    local row = cg.Row({ cg.Vector(0, 1), cg.Vector(20, 1) })
+    for _ = 1, 10 do
+        table.insert(rows, row)
+        row = row:createNext(1)
+    end
 
-local rows = {}
-local row = cg.Row({cg.Vector(0, 1), cg.Vector(20, 1)})
-for _ = 1, 10 do
-    table.insert(rows, row)
-    row = row:createNext(1)
+    lu.assertEquals(#rows, 10)
+    rows[1][1]:assertAlmostEquals(cg.Vector(0, 1))
+    rows[5][1]:assertAlmostEquals(cg.Vector(0, 5))
+    rows[10][1]:assertAlmostEquals(cg.Vector(0, 10))
+
+    -----------------------------------------------------------------------------------
+    local p = cg.RowPatternSkip(1)
+    local orderedRows = {}
+    for _, r in p:iterator(rows) do
+        table.insert(orderedRows, r)
+    end
+    lu.assertEquals(#orderedRows, 10)
+    orderedRows[1][1]:assertAlmostEquals(cg.Vector(0, 1))
+    orderedRows[2][1]:assertAlmostEquals(cg.Vector(0, 3))
+    orderedRows[3][1]:assertAlmostEquals(cg.Vector(0, 5))
+    orderedRows[4][1]:assertAlmostEquals(cg.Vector(0, 7))
+    orderedRows[5][1]:assertAlmostEquals(cg.Vector(0, 9))
+    orderedRows[6][1]:assertAlmostEquals(cg.Vector(0, 10))
+    orderedRows[7][1]:assertAlmostEquals(cg.Vector(0, 8))
+    orderedRows[8][1]:assertAlmostEquals(cg.Vector(0, 6))
+    orderedRows[9][1]:assertAlmostEquals(cg.Vector(0, 4))
+    orderedRows[10][1]:assertAlmostEquals(cg.Vector(0, 2))
+
+    local entries = p:getPossibleEntries(rows)
+    lu.assertEquals(#entries, 8)
+    entries[1].position:assertAlmostEquals(cg.Vector(0, 1))
+    entries[2].position:assertAlmostEquals(cg.Vector(20, 1))
+    entries[3].position:assertAlmostEquals(cg.Vector(0, 10))
+    entries[4].position:assertAlmostEquals(cg.Vector(20, 10))
+    entries[5].position:assertAlmostEquals(cg.Vector(0, 9))
+    entries[6].position:assertAlmostEquals(cg.Vector(20, 9))
+    entries[7].position:assertAlmostEquals(cg.Vector(0, 2))
+    entries[8].position:assertAlmostEquals(cg.Vector(20, 2))
+
+    -----------------------------------------------------------------------------------
+
+    p = cg.RowPatternSkip(2)
+    orderedRows = {}
+    for _, r in p:iterator(rows) do
+        table.insert(orderedRows, r)
+    end
+    lu.assertEquals(#orderedRows, 10)
+    orderedRows[1][1]:assertAlmostEquals(cg.Vector(0, 1))
+    orderedRows[2][1]:assertAlmostEquals(cg.Vector(0, 4))
+    orderedRows[3][1]:assertAlmostEquals(cg.Vector(0, 7))
+    orderedRows[4][1]:assertAlmostEquals(cg.Vector(0, 10))
+    orderedRows[5][1]:assertAlmostEquals(cg.Vector(0, 8))
+    orderedRows[6][1]:assertAlmostEquals(cg.Vector(0, 5))
+    orderedRows[7][1]:assertAlmostEquals(cg.Vector(0, 2))
+    orderedRows[8][1]:assertAlmostEquals(cg.Vector(0, 3))
+    orderedRows[9][1]:assertAlmostEquals(cg.Vector(0, 6))
+    orderedRows[10][1]:assertAlmostEquals(cg.Vector(0, 9))
+
+    entries = p:getPossibleEntries(rows)
+    lu.assertEquals(#entries, 8)
+    entries[1].position:assertAlmostEquals(cg.Vector(0, 1))
+    entries[2].position:assertAlmostEquals(cg.Vector(20, 1))
+    entries[3].position:assertAlmostEquals(cg.Vector(0, 10))
+    entries[4].position:assertAlmostEquals(cg.Vector(20, 10))
+    entries[5].position:assertAlmostEquals(cg.Vector(0, 2))
+    entries[6].position:assertAlmostEquals(cg.Vector(20, 2))
+    entries[7].position:assertAlmostEquals(cg.Vector(0, 9))
+    entries[8].position:assertAlmostEquals(cg.Vector(20, 9))
+
+    --------------------------------------------------------------------------------------
+    table.insert(rows, rows[#rows]:createNext(1))
+    --------------------------------------------------------------------------------------
+
+    p = cg.RowPatternSkip(1)
+    orderedRows = {}
+    for _, r in p:iterator(rows) do
+        table.insert(orderedRows, r)
+    end
+    lu.assertEquals(#orderedRows, 11)
+    orderedRows[1][1]:assertAlmostEquals(cg.Vector(0, 1))
+    orderedRows[2][1]:assertAlmostEquals(cg.Vector(0, 3))
+    orderedRows[3][1]:assertAlmostEquals(cg.Vector(0, 5))
+    orderedRows[4][1]:assertAlmostEquals(cg.Vector(0, 7))
+    orderedRows[5][1]:assertAlmostEquals(cg.Vector(0, 9))
+    orderedRows[6][1]:assertAlmostEquals(cg.Vector(0, 11))
+    orderedRows[7][1]:assertAlmostEquals(cg.Vector(0, 10))
+    orderedRows[8][1]:assertAlmostEquals(cg.Vector(0, 8))
+    orderedRows[9][1]:assertAlmostEquals(cg.Vector(0, 6))
+    orderedRows[10][1]:assertAlmostEquals(cg.Vector(0, 4))
+    orderedRows[11][1]:assertAlmostEquals(cg.Vector(0, 2))
+
+    entries = p:getPossibleEntries(rows)
+    lu.assertEquals(#entries, 8)
+    entries[1].position:assertAlmostEquals(cg.Vector(0, 1))
+    entries[2].position:assertAlmostEquals(cg.Vector(20, 1))
+    entries[3].position:assertAlmostEquals(cg.Vector(0, 11))
+    entries[4].position:assertAlmostEquals(cg.Vector(20, 11))
+    entries[5].position:assertAlmostEquals(cg.Vector(0, 10))
+    entries[6].position:assertAlmostEquals(cg.Vector(20, 10))
+    entries[7].position:assertAlmostEquals(cg.Vector(0, 2))
+    entries[8].position:assertAlmostEquals(cg.Vector(20, 2))
+
+    -----------------------------------------------------------------------------------
+
+    p = cg.RowPatternSkip(2)
+    orderedRows = {}
+    for _, r in p:iterator(rows) do
+        table.insert(orderedRows, r)
+    end
+    lu.assertEquals(#orderedRows, 11)
+    orderedRows[1][1]:assertAlmostEquals(cg.Vector(0, 1))
+    orderedRows[2][1]:assertAlmostEquals(cg.Vector(0, 4))
+    orderedRows[3][1]:assertAlmostEquals(cg.Vector(0, 7))
+    orderedRows[4][1]:assertAlmostEquals(cg.Vector(0, 10))
+    orderedRows[5][1]:assertAlmostEquals(cg.Vector(0, 11))
+    orderedRows[6][1]:assertAlmostEquals(cg.Vector(0, 8))
+    orderedRows[7][1]:assertAlmostEquals(cg.Vector(0, 5))
+    orderedRows[8][1]:assertAlmostEquals(cg.Vector(0, 2))
+    orderedRows[9][1]:assertAlmostEquals(cg.Vector(0, 3))
+    orderedRows[10][1]:assertAlmostEquals(cg.Vector(0, 6))
+    orderedRows[11][1]:assertAlmostEquals(cg.Vector(0, 9))
+
+    entries = p:getPossibleEntries(rows)
+    lu.assertEquals(#entries, 8)
+    entries[1].position:assertAlmostEquals(cg.Vector(0, 1))
+    entries[2].position:assertAlmostEquals(cg.Vector(20, 1))
+    entries[3].position:assertAlmostEquals(cg.Vector(0, 11))
+    entries[4].position:assertAlmostEquals(cg.Vector(20, 11))
+    entries[5].position:assertAlmostEquals(cg.Vector(0, 3))
+    entries[6].position:assertAlmostEquals(cg.Vector(20, 3))
+    entries[7].position:assertAlmostEquals(cg.Vector(0, 9))
+    entries[8].position:assertAlmostEquals(cg.Vector(20, 9))
 end
-
-lu.assertEquals(#rows, 10)
-rows[1][1]:assertAlmostEquals(cg.Vector(0, 1))
-rows[5][1]:assertAlmostEquals(cg.Vector(0, 5))
-rows[10][1]:assertAlmostEquals(cg.Vector(0, 10))
-
------------------------------------------------------------------------------------
-local p = cg.RowPatternSkip(1)
-local orderedRows = {}
-for _, r in p:iterator(rows) do
-    table.insert(orderedRows, r)
-end
-lu.assertEquals(#orderedRows, 10)
-orderedRows[1][1]:assertAlmostEquals(cg.Vector(0, 1))
-orderedRows[2][1]:assertAlmostEquals(cg.Vector(0, 3))
-orderedRows[3][1]:assertAlmostEquals(cg.Vector(0, 5))
-orderedRows[4][1]:assertAlmostEquals(cg.Vector(0, 7))
-orderedRows[5][1]:assertAlmostEquals(cg.Vector(0, 9))
-orderedRows[6][1]:assertAlmostEquals(cg.Vector(0, 10))
-orderedRows[7][1]:assertAlmostEquals(cg.Vector(0, 8))
-orderedRows[8][1]:assertAlmostEquals(cg.Vector(0, 6))
-orderedRows[9][1]:assertAlmostEquals(cg.Vector(0, 4))
-orderedRows[10][1]:assertAlmostEquals(cg.Vector(0, 2))
-
-local entries = p:getPossibleEntries(rows)
-lu.assertEquals(#entries, 8)
-entries[1].position:assertAlmostEquals(cg.Vector(0, 1))
-entries[2].position:assertAlmostEquals(cg.Vector(20, 1))
-entries[3].position:assertAlmostEquals(cg.Vector(0, 10))
-entries[4].position:assertAlmostEquals(cg.Vector(20, 10))
-entries[5].position:assertAlmostEquals(cg.Vector(0, 9))
-entries[6].position:assertAlmostEquals(cg.Vector(20, 9))
-entries[7].position:assertAlmostEquals(cg.Vector(0, 2))
-entries[8].position:assertAlmostEquals(cg.Vector(20, 2))
-
------------------------------------------------------------------------------------
-
-p = cg.RowPatternSkip(2)
-orderedRows = {}
-for _, r in p:iterator(rows) do
-    table.insert(orderedRows, r)
-end
-lu.assertEquals(#orderedRows, 10)
-orderedRows[1][1]:assertAlmostEquals(cg.Vector(0, 1))
-orderedRows[2][1]:assertAlmostEquals(cg.Vector(0, 4))
-orderedRows[3][1]:assertAlmostEquals(cg.Vector(0, 7))
-orderedRows[4][1]:assertAlmostEquals(cg.Vector(0, 10))
-orderedRows[5][1]:assertAlmostEquals(cg.Vector(0, 8))
-orderedRows[6][1]:assertAlmostEquals(cg.Vector(0, 5))
-orderedRows[7][1]:assertAlmostEquals(cg.Vector(0, 2))
-orderedRows[8][1]:assertAlmostEquals(cg.Vector(0, 3))
-orderedRows[9][1]:assertAlmostEquals(cg.Vector(0, 6))
-orderedRows[10][1]:assertAlmostEquals(cg.Vector(0, 9))
-
-entries = p:getPossibleEntries(rows)
-lu.assertEquals(#entries, 8)
-entries[1].position:assertAlmostEquals(cg.Vector(0, 1))
-entries[2].position:assertAlmostEquals(cg.Vector(20, 1))
-entries[3].position:assertAlmostEquals(cg.Vector(0, 10))
-entries[4].position:assertAlmostEquals(cg.Vector(20, 10))
-entries[5].position:assertAlmostEquals(cg.Vector(0, 2))
-entries[6].position:assertAlmostEquals(cg.Vector(20, 2))
-entries[7].position:assertAlmostEquals(cg.Vector(0, 9))
-entries[8].position:assertAlmostEquals(cg.Vector(20, 9))
-
---------------------------------------------------------------------------------------
-table.insert(rows, rows[#rows]:createNext(1))
---------------------------------------------------------------------------------------
-
-p = cg.RowPatternSkip(1)
-orderedRows = {}
-for _, r in p:iterator(rows) do
-    table.insert(orderedRows, r)
-end
-lu.assertEquals(#orderedRows, 11)
-orderedRows[1][1]:assertAlmostEquals(cg.Vector(0, 1))
-orderedRows[2][1]:assertAlmostEquals(cg.Vector(0, 3))
-orderedRows[3][1]:assertAlmostEquals(cg.Vector(0, 5))
-orderedRows[4][1]:assertAlmostEquals(cg.Vector(0, 7))
-orderedRows[5][1]:assertAlmostEquals(cg.Vector(0, 9))
-orderedRows[6][1]:assertAlmostEquals(cg.Vector(0, 11))
-orderedRows[7][1]:assertAlmostEquals(cg.Vector(0, 10))
-orderedRows[8][1]:assertAlmostEquals(cg.Vector(0, 8))
-orderedRows[9][1]:assertAlmostEquals(cg.Vector(0, 6))
-orderedRows[10][1]:assertAlmostEquals(cg.Vector(0, 4))
-orderedRows[11][1]:assertAlmostEquals(cg.Vector(0, 2))
-
-entries = p:getPossibleEntries(rows)
-lu.assertEquals(#entries, 8)
-entries[1].position:assertAlmostEquals(cg.Vector(0, 1))
-entries[2].position:assertAlmostEquals(cg.Vector(20, 1))
-entries[3].position:assertAlmostEquals(cg.Vector(0, 11))
-entries[4].position:assertAlmostEquals(cg.Vector(20, 11))
-entries[5].position:assertAlmostEquals(cg.Vector(0, 10))
-entries[6].position:assertAlmostEquals(cg.Vector(20, 10))
-entries[7].position:assertAlmostEquals(cg.Vector(0, 2))
-entries[8].position:assertAlmostEquals(cg.Vector(20, 2))
-
------------------------------------------------------------------------------------
-
-p = cg.RowPatternSkip(2)
-orderedRows = {}
-for _, r in p:iterator(rows) do
-    table.insert(orderedRows, r)
-end
-lu.assertEquals(#orderedRows, 11)
-orderedRows[1][1]:assertAlmostEquals(cg.Vector(0, 1))
-orderedRows[2][1]:assertAlmostEquals(cg.Vector(0, 4))
-orderedRows[3][1]:assertAlmostEquals(cg.Vector(0, 7))
-orderedRows[4][1]:assertAlmostEquals(cg.Vector(0, 10))
-orderedRows[5][1]:assertAlmostEquals(cg.Vector(0, 11))
-orderedRows[6][1]:assertAlmostEquals(cg.Vector(0, 8))
-orderedRows[7][1]:assertAlmostEquals(cg.Vector(0, 5))
-orderedRows[8][1]:assertAlmostEquals(cg.Vector(0, 2))
-orderedRows[9][1]:assertAlmostEquals(cg.Vector(0, 3))
-orderedRows[10][1]:assertAlmostEquals(cg.Vector(0, 6))
-orderedRows[11][1]:assertAlmostEquals(cg.Vector(0, 9))
-
-entries = p:getPossibleEntries(rows)
-lu.assertEquals(#entries, 8)
-entries[1].position:assertAlmostEquals(cg.Vector(0, 1))
-entries[2].position:assertAlmostEquals(cg.Vector(20, 1))
-entries[3].position:assertAlmostEquals(cg.Vector(0, 11))
-entries[4].position:assertAlmostEquals(cg.Vector(20, 11))
-entries[5].position:assertAlmostEquals(cg.Vector(0, 3))
-entries[6].position:assertAlmostEquals(cg.Vector(20, 3))
-entries[7].position:assertAlmostEquals(cg.Vector(0, 9))
-entries[8].position:assertAlmostEquals(cg.Vector(20, 9))
+os.exit(lu.LuaUnit.run())
